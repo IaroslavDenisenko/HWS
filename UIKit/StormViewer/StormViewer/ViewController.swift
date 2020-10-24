7d21cbf896b5e4b2f0b6ceaf85436187d7dfccc9
Iaroslav Denisenko <yaroslav1985@gmail.com>
2020-10-24 17:33:50 +0300

    Project1 feat: image shown counter with saving in UserDefaults

--- UIKit/StormViewer/StormViewer/ViewController.swift
+++ UIKit/StormViewer/StormViewer/ViewController.swift
@@ -11,14 +11,19 @@ import UIKit
 class ViewController: UITableViewController {
     
     var pictures = [String]()
-
+    var counts = [Int]()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Storm Viewer"
+        loadCounts()
         navigationController?.navigationBar.prefersLargeTitles = true
         performSelector(inBackground: #selector(loadPictures), with: nil)
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "recommend the app", style: .done, target: self, action: #selector(recommendButtonTapped))
         
+    }
+    func loadCounts() {
+        counts = UserDefaults.standard.array(forKey: "counts") as? [Int] ?? Array(repeating: 0, count: 10)
     }
     
     @objc func recommendButtonTapped() {
@@ -50,16 +55,21 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PictureCell", for: indexPath)
         let pictureName = self.pictures[indexPath.row]
+        let count = counts[indexPath.row]
         cell.textLabel?.text = pictureName
+        cell.detailTextLabel?.text = "Shown: \(count)"
         cell.imageView?.image = UIImage(imageLiteralResourceName: pictureName)
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let detailVC = storyboard?.instantiateViewController(withIdentifier: "DetailVC") as? DetailViewController else { return }
+        counts[indexPath.row] += 1
+        UserDefaults.standard.set(counts, forKey: "counts")
         detailVC.selectedImage = pictures[indexPath.row]
         detailVC.titleName = "Picture \(indexPath.row + 1) of \(pictures.count)"
         navigationController?.pushViewController(detailVC, animated: true)
+        tableView.reloadRows(at: [indexPath], with: .automatic)
     }
 
 }
