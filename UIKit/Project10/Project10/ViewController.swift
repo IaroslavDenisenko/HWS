bcd47585458fedda8073cdd3e3d6f5651e6d7e04
Iaroslav Denisenko <yaroslav1985@gmail.com>
2020-12-31 18:48:42 +0300

    Project 10: biometric authentication has been added

--- UIKit/Project10/Project10/ViewController.swift
+++ UIKit/Project10/Project10/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
@@ -15,8 +16,14 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadPeople()
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(selectPicture))
+        setupButtons()
+        forceAuthentication()
+    }
+    
+    func setupButtons() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add picture", style: .plain, target: self, action: #selector(selectPicture))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Authentication", style: .plain, target: self, action: #selector(forceAuthentication))
+        title = "Pictures"
     }
     
     @objc func selectPicture() {
@@ -37,6 +44,32 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         }
     }
     
+    @objc func forceAuthentication() {
+        let context = LAContext()
+        var error: NSError?
+        let reason = "Authenticate yourself!"
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] (success, error) in
+                DispatchQueue.main.async {
+                    if success {
+                        self?.loadPeople()
+                        self?.collectionView.reloadData()
+                    } else {
+                        self?.showAlert(title: "Authentication failed", message: "You could not be verified; please try again.", actionTitle: "OK")
+                    }
+                }
+            }
+        } else {
+            showAlert(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", actionTitle: "OK")
+        }
+    }
+    
+    func showAlert(title: String?, message: String?, actionTitle: String?, handler: ((UIAlertAction) -> Void)? = nil) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: actionTitle, style: .default, handler: handler))
+        present(ac, animated: true)
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         people.count
     }
@@ -47,7 +80,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         }
         cell.name.text = people[indexPath.item].name
         let imageName = people[indexPath.item].image
-        let imagePath = getDocumentsPath().appendingPathComponent(imageName)
+        let imagePath = self.getDocumentsPath().appendingPathComponent(imageName)
         cell.imageView.image = UIImage(contentsOfFile: imagePath.path)
         cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.4).cgColor
         cell.imageView.layer.borderWidth = 2
@@ -84,6 +117,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     func deleteItem(action: UIAlertAction!) {
         people.remove(at: index)
         collectionView.reloadData()
+        save()
     }
     
     func save() {
